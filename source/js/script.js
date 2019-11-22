ebf251ac839fecdbee65d2998fbeda9cf2119f66
Sergey <54816584+sergeydushechkin@users.noreply.github.com>
2019-11-22 20:30:37 +0300

    Добавил показ и скрытие меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,18 @@
 var mobileToggle = document.querySelector(".page-header__toggle");
+var mobileMenu = document.querySelector(".menu-nav__list");
+
+// mobileMenu.classList.add("menu-nav__list--close");
+// mobileToggle.classList.add("page-header__toggle--open")
 
 mobileToggle.addEventListener("click", function(evt) {
   evt.preventDefault();
   if (mobileToggle.classList.contains("page-header__toggle--open")) {
     mobileToggle.classList.remove("page-header__toggle--open");
     mobileToggle.classList.add("page-header__toggle--close");
+    mobileMenu.classList.remove("menu-nav__list--close")
   } else {
     mobileToggle.classList.remove("page-header__toggle--close");
     mobileToggle.classList.add("page-header__toggle--open");
+    mobileMenu.classList.add("menu-nav__list--close")
   }
 });
